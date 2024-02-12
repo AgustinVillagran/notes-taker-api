3502178bef6bd785c1a26145af8a2814f63a9f39
Agustin Villagran <magustin.villagran@gmail.com>
2024-02-12 13:47:33 -0300

    feat(main): added env management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
+const PORT = process.env.PORT;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create(AppModule, {});
+  await app.listen(PORT);
+  logger.log(`App running in port ${PORT} 🚀`);
 }
 bootstrap();
